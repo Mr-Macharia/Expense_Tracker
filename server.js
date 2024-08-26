59517a8e0f07f874db94fb736affe1cc8bcc543c
Mr.Macharia <gichogumacharia001@gmail.com>
2024-08-26 14:01:49 +0300

    Changed the port number

--- server.js
+++ server.js
@@ -146,6 +146,6 @@ app.get('*', (req, res) => {
 });
 
 // Start the Server
-app.listen(3000, () => {
-    console.log('Server running on port 3000');
+app.listen(5000, () => {
+    console.log('Server running on port 5000');
 });
